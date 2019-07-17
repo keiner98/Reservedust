25b97396fb07e2c3172880d1aef84a91d36482f8
[Cwirl] <[cwilmer@uninorte.edu.co]>
2019-07-17 03:06:32 -0500

    web app in angular

--- api/routes/motelRecords.js
+++ api/routes/motelRecords.js
@@ -30,12 +30,12 @@ router.post("/login", (req, res) => {
     (err, user) => {
       if (err) throw err;
       if (!user.length) {
-        res.status(401).send("Usuario invalido");
+        res.status(401).json({ ans: "Usuario invalido" });
       } else {
         if (user[0].contraseña !== contraseña) {
-          res.status(401).send("Contraseña invalida");
+          res.status(401).json({ ans: "Contraseña invalida" });
         } else {
-          res.status(200).send(user[0]);
+          res.status(200).json({ ans: "success", data: user[0] });
         }
       }
     }
@@ -45,34 +45,53 @@ router.post("/login", (req, res) => {
 
 /**
  * Registro queries
- * table: usuario 
- * INSERT INTO usuario (usuario, contraseña, tipoUsuario) VALUES ('usuario3@hotmail.com', 'user123', 2); 
- * 
+ * table: usuario
+ * INSERT INTO usuario (usuario, contraseña, tipoUsuario) VALUES ('usuario3@hotmail.com', 'user123', 2);
+ *
  * table: motel
  * INSERT INTO motel (idmotel, nombreMotel, telefono, estado, latitud, longitud)
  * VALUES ((SELECT MAX(idusuario) from usuario),'Casa Nuba', '4251232', 1,'11.23274937841482', '-74.1765546798706');
  */
 
-
 router.post("/signup", (req, res) => {
-  const { usuario, contraseña, tipoUsuario, nombreMotel, telefono, estado, latitud, longitud } = req.body;
+  const {
+    usuario,
+    contraseña,
+    tipoUsuario,
+    nombreMotel,
+    telefono,
+    estado,
+    latitud,
+    longitud
+  } = req.body;
   db = conect();
   const user = [usuario, contraseña, tipoUsuario];
-  const motel = [nombreMotel, telefono, estado, latitud, longitud]
-  db.query(`SELECT * FROM usuario where usuario = '${usuario}'`, (err, results) => {
-    if (err) throw err;
-    if (results.length){
-      res.send("Usuario ya esta registrado")
-    } else {
-      db.query("INSERT INTO usuario (usuario, contraseña, tipoUsuario) VALUES (?, ?, ?)", user, (err, user) => {
-        if (err) throw err;
-        db.query("INSERT INTO motel (idusuario, nombreMotel, telefono, estado, latitud, longitud) VALUES ((SELECT MAX(idusuario) from usuario),?,?,?,?,?)", motel, (err, user) => {
-          if (err) throw err;
-          res.status(200).send("Motel registrado");
-        });
-        db.end();
-      });
+  const motel = [nombreMotel, telefono, estado, latitud, longitud];
+  db.query(
+    `SELECT * FROM usuario where usuario = '${usuario}'`,
+    (err, results) => {
+      if (err) throw err;
+      if (results.length) {
+        res.send("Usuario ya esta registrado");
+      } else {
+        db.query(
+          "INSERT INTO usuario (usuario, contraseña, tipoUsuario) VALUES (?, ?, ?)",
+          user,
+          (err, user) => {
+            if (err) throw err;
+            db.query(
+              "INSERT INTO motel (idusuario, nombreMotel, telefono, estado, latitud, longitud) VALUES ((SELECT MAX(idusuario) from usuario),?,?,?,?,?)",
+              motel,
+              (err, user) => {
+                if (err) throw err;
+                res.status(200).send("Motel registrado");
+              }
+            );
+            db.end();
+          }
+        );
+      }
     }
-  })
+  );
 });
 module.exports = router;
